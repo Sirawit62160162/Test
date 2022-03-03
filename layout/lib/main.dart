51bf84c1c0f8890a4b350e731853e901cba2c7cf
rungjaroen <73059254+rungjaroen@users.noreply.github.com>
2022-03-03 13:53:02 +0700

    aum

--- layout/lib/main.dart
+++ layout/lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "MyApp",
+      title: "aum",
       home: Homepage(), // called home.dart
     );
   }
